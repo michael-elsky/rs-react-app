ebeac7cf9bc5b72af384e8a75b6907a30d70564a
Michael <qwentin536@gmail.com>
2026-04-29 22:47:12 +0300

    feat: create Search class component with form UI 2026-04-29 22:47:12

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-function App() {
-  return <></>;
+import './global.css';
+
+import { Component } from 'react';
+
+import Search from './components/Search';
+import Main from './components/Main';
+
+class App extends Component {
+  render() {
+    return (
+      <Main>
+        <Search />
+      </Main>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,29 @@
+import classes from './Search.module.css';
+
+import { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <form className={classes.app__form}>
+        <label
+          className={classes.app__label}
+          htmlFor="searchInput"
+        >
+          Search
+        </label>
+        <input
+          className={classes.app__input}
+          type="text"
+          id="searchInput"
+          placeholder="Search items..."
+        />
+        <button className={classes['app__search-btn']} type="submit">
+          Search
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Search;
