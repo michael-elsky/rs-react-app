2f4a9aef2c4c725c40466c187e7ff79e9f60e9d0
Michael <qwentin536@gmail.com>
2026-04-30 14:43:43 +0300

    refactor: add index file for TestError re-export 2026-04-30 14:43:43

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Component } from 'react';
 import Search from './components/Search';
 import Main from './components/Main';
 import Result from './components/Result';
-import TestError from './components/TestError/TestError';
+import TestError from './components/TestError';
 
 class App extends Component {
   render() {
