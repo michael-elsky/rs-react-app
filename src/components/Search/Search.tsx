7bb48b5ca6b5432cb38b648cd87c4c3a5260a3a4
Michael <qwentin536@gmail.com>
2026-05-13 20:35:25 +0300

    refactor: convert Search class component to functional component 2026-05-13 20:35:25

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,28 +1,25 @@
 import Button from '../Ui/Button/Button';
 import classes from './Search.module.css';
 
-import { Component } from 'react';
 import type { SearchProps } from './Search.types';
 
-class Search extends Component<SearchProps> {
-  render() {
-    return (
-      <form className={classes.app__form} onSubmit={this.props.handleSubmit}>
-        <label className={classes.app__label} htmlFor="searchInput">
-          Search
-        </label>
-        <input
-          className={classes.app__input}
-          type="text"
-          id="searchInput"
-          name="searchInput"
-          placeholder="Search items..."
-          defaultValue={this.props.searchInputValue}
-        />
-        <Button className={classes['app__search-btn']}>Search</Button>
-      </form>
-    );
-  }
-}
+const Search = ({ handleSubmit, searchInputValue }: SearchProps) => {
+  return (
+    <form className={classes.app__form} onSubmit={handleSubmit}>
+      <label className={classes.app__label} htmlFor="searchInput">
+        Search
+      </label>
+      <input
+        className={classes.app__input}
+        type="text"
+        id="searchInput"
+        name="searchInput"
+        placeholder="Search items..."
+        defaultValue={searchInputValue}
+      />
+      <Button className={classes['app__search-btn']}>Search</Button>
+    </form>
+  );
+};
 
 export default Search;
