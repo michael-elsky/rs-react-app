1a4b73abe9d070b95fd0f5bcaaf8d275abf4de66
Michael <qwentin536@gmail.com>
2026-05-13 20:19:32 +0300

    refactor: convert ErrorDisplay class component to functional component 2026-05-13 20:19:32

--- src/components/ErrorDisplay/ErrorDisplay.tsx
+++ src/components/ErrorDisplay/ErrorDisplay.tsx
@@ -1,32 +1,25 @@
 import classes from './ErrorDisplay.module.css';
 
-import { Component } from 'react';
-
 import type { ErrorDisplayProps } from './ErrorDisplay.types';
 import Button from '../Ui/Button/Button';
 
-class ErrorDisplay extends Component<ErrorDisplayProps> {
-  render() {
-    if (this.props.hasError) {
-      return (
-        <>
-          <p className={classes['app__error-message']}>
-            {this.props.errorMessage}
-          </p>
-          <Button
-            className={classes['app__reset-error-btn']}
-            onClick={this.props.onReset}
-          >
-            Reset error
-          </Button>
-        </>
-      );
-    }
-
+const ErrorDisplay = ({
+  hasError,
+  errorMessage,
+  onReset,
+}: ErrorDisplayProps) => {
+  if (hasError) {
     return (
-      <p className={classes['app__error-message']}>{this.props.errorMessage}</p>
+      <>
+        <p className={classes['app__error-message']}>{errorMessage}</p>
+        <Button className={classes['app__reset-error-btn']} onClick={onReset}>
+          Reset error
+        </Button>
+      </>
     );
   }
-}
+
+  return <p className={classes['app__error-message']}>{errorMessage}</p>;
+};
 
 export default ErrorDisplay;
