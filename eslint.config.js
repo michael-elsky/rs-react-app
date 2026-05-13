97f7c798ed9cca5a92864cb741245cb30e26bf2a
Michael <qwentin536@gmail.com>
2026-05-13 20:08:18 +0300

    chore: add eslint-plugin-react-hooks 2026-05-13 20:08:18

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 export default defineConfig([
   globalIgnores(['dist', 'coverage']),
@@ -10,6 +11,7 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
     ],
     languageOptions: {
       globals: globals.browser,
