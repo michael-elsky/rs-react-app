ee9b345ac744a4caae4d8b8f2ae39d7c82646350
Michael <qwentin536@gmail.com>
2026-05-15 21:20:54 +0300

    feat: add ResultDetails placeholder component 2026-05-15 21:20:54

---
+++ src/components/Result/ResultDetail/ResultDetails.tsx
@@ -0,0 +1,12 @@
+import classes from './ResultDetail.module.css';
+
+const ResultDetails = () => {
+  return (
+    <section className={classes['app__result-detail']}>
+      <h1>Detail page</h1>
+      <p>Description</p>
+    </section>
+  );
+};
+
+export default ResultDetails;
