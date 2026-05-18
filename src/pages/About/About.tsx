99349214432a9d478b161f55b02fe4cab96b4692
Michael <qwentin536@gmail.com>
2026-05-19 00:05:55 +0300

    feat: add information to About page 2026-05-19 00:05:55

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,7 +1,25 @@
 const About = () => {
   return (
     <section>
-      <h1>About</h1>
+      <h1>Michael Elsky</h1>
+      <p>
+        <a
+          href="https://github.com/michael-elsky"
+          target="_blank"
+          rel="noopener"
+        >
+          GitHub
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://rs.school/courses/reactjs"
+          target="_blank"
+          rel="noopener"
+        >
+          RS School React course
+        </a>
+      </p>
     </section>
   );
 };
