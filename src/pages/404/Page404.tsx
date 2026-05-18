0806dd0b39b9d5106b43fe01611c07073e9a1199
Michael <qwentin536@gmail.com>
2026-05-18 23:53:52 +0300

    style: improve 404 page styling 2026-05-18 23:53:52

--- src/pages/404/Page404.tsx
+++ src/pages/404/Page404.tsx
@@ -1,5 +1,7 @@
+import classes from './Page404.module.css';
+
 const Page404 = () => {
-  return <h1>Page 404</h1>;
+  return <h1 className={classes['app__404-title']}>Page 404</h1>;
 };
 
 export default Page404;
