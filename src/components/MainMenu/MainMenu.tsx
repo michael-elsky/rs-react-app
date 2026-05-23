78b3a8ea6a284130ec3a2ed97effc042b30e03f7
Michael <qwentin536@gmail.com>
2026-05-23 17:00:08 +0300

    style: update navigation menu styles 2026-05-23 17:00:08

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,14 +1,33 @@
 import classes from './MainMenu.module.css';
 
 import { NavLink } from 'react-router-dom';
+
 const MainMenu = () => {
   return (
     <ul className={classes.app_menu}>
       <li>
-        <NavLink to={'/'}>Home</NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? `${classes['app__menu-link']} ${classes['app__menu-link--active']}`
+              : classes['app__menu-link']
+          }
+          to={'/'}
+        >
+          Home
+        </NavLink>
       </li>
       <li>
-        <NavLink to={'/about'}>About</NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? `${classes['app__menu-link']} ${classes['app__menu-link--active']}`
+              : classes['app__menu-link']
+          }
+          to={'/about'}
+        >
+          About
+        </NavLink>
       </li>
     </ul>
   );
