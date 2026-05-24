cf4912ca042fbe11b144b25fa76c61d5b06f02bb
Michael <qwentin536@gmail.com>
2026-05-24 22:45:45 +0300

    fix: update theme handling according to project requirements 2026-05-24 22:45:45

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 import type { ChildrenProp } from '../types/types';
 import type { Theme, ThemeContextType } from './ThemeContext.types';
@@ -13,11 +13,12 @@ export const ThemeContext = createContext<ThemeContextType>(initialValue);
 const ThemeContextProvider = ({ children }: ChildrenProp) => {
   const [theme, setTheme] = useState<Theme>('light');
 
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
   const toggleTheme = () => {
     setTheme((prevState) => (prevState === 'light' ? 'dark' : 'light'));
-
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    document.body.className = newTheme;
   };
 
   return (
