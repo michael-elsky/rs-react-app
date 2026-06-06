b11b9f4b1596d01f04770d7a958b972d7e5f5a68
Michael <qwentin536@gmail.com>
2026-06-06 21:58:07 +0300

    refactor: rename Forms to Playground 2026-06-06 21:58:07

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,5 @@
 import Entries from '../Entries/Entries'
-import Forms from '../Playground/Playground'
+import Playground from '../Playground/Playground'
 import './Main.css'
 
 const Main = () => {
@@ -7,7 +7,7 @@ const Main = () => {
     <main className="app__main">
       <div className="container">
         <div className="app__main-wrapper">
-          <Forms />
+          <Playground />
 
           <Entries />
         </div>
